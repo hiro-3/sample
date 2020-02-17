aaa32f23fc2f6211eeac39ac0c3f2b6cde539486
hiro <hiro.chibita@gmail.com>
2020-02-17 14:20:33 +0000

    課題２

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_142759) do
+ActiveRecord::Schema.define(version: 2020_02_17_120445) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
