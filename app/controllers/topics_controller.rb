97098bfc45aa62a9e4ad8c5be1e5f84b741cd312
hiro <hiro.chibita@gmail.com>
2020-03-01 13:43:42 +0000

    いいね

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,17 +1,17 @@
 class TopicsController < ApplicationController
   def index
-    @topic = Topic.all
+    @topics = Topic.all.includes(:favorite_users)
   end 
     
     def new
-    @topic = Topic.new
+    @topics = Topic.new
   end
 
  def create
-   @topic = current_user.topics.new(topic_params)
+   @topics = current_user.topics.new(topic_params)
 
- if @topic.save
-   redirect_to topics_path, success: '投稿に失敗しました'
+ if @topics.save
+   redirect_to topics_path, success: '投稿に成功しました'
  else
    flash.now[:danger] = "投稿に失敗しました"
    render :new
