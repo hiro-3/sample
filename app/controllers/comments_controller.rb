7a2b9cf87154f09898655f2fcfd35e77230168d5
hiro <hiro.chibita@gmail.com>
2020-03-04 08:24:19 +0000

    コメント

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,25 @@
 class CommentsController < ApplicationController
+  
   def new
-    @comment = Comment.new
-  end
-    
-  def create
-    @comment = Comment.new(comments_params)
-    
+   @comment = Comment.new
+  end 
+  
+  
+   def create
+   @comment = current_user.(comment_params)
+   comments.user_id = current_user.id
+   comments.topic_id = params[:topic_id]
+   
     if @comment.save
       redirect_to favorite_path, success: "コメントをしました"
     else
       flash.now[:danger] = "コメントできませんでした"
       render :new
     end
-  end
-   private
-    def comments_params
-      params.require(:comment).permit(:body)
-    end
-  
-  
-  
-   
 end    
+ 
+   private
+    def comment_params
+      params.require(:comment).permit(:body, :user_id ,:topic_id)
+    end
+end 
